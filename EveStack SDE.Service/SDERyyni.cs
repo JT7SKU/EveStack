f9334ce1fa446dda60818fcafc1bd3dbcfedd5db
Janisku7 <janisku7@gmail.com>
2025-12-27 22:27:07 +0200

    stack started of eve

---
+++ EveStack SDE.Service/SDERyyni.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+namespace EveStack_SDE.Service
+{
+    public class SDERyyni
+    {
+        private string latestbuild = "developers.eveonline.com/static-data/tranquility/latest.jsonl";
+        private string sde = "";
+        public Task GetLatestBuild()
+        {
+            try
+            {
+                sde = ReadJsonl(latestbuild);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+            return Task.CompletedTask;
+        }
+
+        private string ReadJsonl(string latestbuild)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
